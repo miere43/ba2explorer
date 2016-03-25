f209e8449e91d5569db19bc7cd28c119ed393acd
miere43 <x.miere@gmail.com>
2016-03-25 21:26:25 +0300

    Going to fix Ba2Tools now.

---
+++ Source/Ba2Explorer/Ba2Explorer/FilePreview.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ba2Explorer
+{
+
+
+    public partial class FilePreview : UserControl
+    {
+        private enum FileType
+        {
+            Unknown,
+            Text
+        }
+
+        public FilePreview()
+        {
+            InitializeComponent();
+        }
+
+        public void SetPreviewTarget(Stream stream, string fileType)
+        {
+            
+        }
+
+        private FileType ResolveFileTypeFromExtension(string extension)
+        {
+            if (extension == "txt")
+            {
+                return FileType.Text;
+            }
+
+            return FileType.Unknown;
+        }
+    }
+}
